d67e9d170859e7531f530b745a8d301fedb53c8a
charb <30449853+aleccharb@users.noreply.github.com>
2017-10-26 22:48:37 -0400

    lkn

--- WebbyWeb/Models/Habit.cs
+++ WebbyWeb/Models/Habit.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 namespace WebbyWeb.Models
 {
     public class Habit
     {
-        public Habit()
-        {
-        }
+        [Required]
+        public Dictionary<string,List<int>> Habits { get; set; }    //dictionary key is habit name, value is list of integers
+                                                                    //list[0] is habit type, rest are times per day
+
+
     }
 }
+//making changes to see if it gets added
