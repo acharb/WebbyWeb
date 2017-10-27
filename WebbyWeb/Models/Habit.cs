738ae1c8c7597e43be7da4defe34c7f6c7ca259b
charb <30449853+aleccharb@users.noreply.github.com>
2017-10-26 22:16:13 -0400

    committing

---
+++ WebbyWeb/Models/Habit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebbyWeb.Models
+{
+    public class Habit
+    {
+        public Habit()
+        {
+        }
+    }
+}
---
+++ WebbyWeb/wwwroot/js/songscript.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebbyWeb.wwwroot.js
+{
+    public class songscript
+    {
+        public songscript()
+        {
+        }
+    }
+}
