a0fb4632842439fa2febbd3c633d560c95c19d41
charb <30449853+aleccharb@users.noreply.github.com>
2017-10-27 16:33:01 -0400

    adf

--- WebbyWeb/Models/Habit.cs
+++ WebbyWeb/Models/Habit.cs
@@ -10,6 +10,7 @@ namespace WebbyWeb.Models
         public string Name { get; set; }                                                            
         public TimeSpan[] Times { get; set; }
         public string Description { get; set; }
+        
 
     }
 }
