fe8687d9627212b1e76657687b9bd6d9597647f8
charb <30449853+aleccharb@users.noreply.github.com>
2017-10-26 22:21:21 -0400

    commit

--- WebbyWeb/wwwroot/js/EmbedSpotify.js
+++ WebbyWeb/wwwroot/js/EmbedSpotify.js
@@ -1,1 +1,23 @@
-﻿
+﻿/* Load the HTTP library */
+var http = require("http");
+
+/* Create an HTTP server to handle responses */
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
+
+
+var counter=true;
+
+$("#button").click(function(){
+if(counter){
+    document.getElementById('spotify').height="400";
+    counter=false;
+    }
+else{
+    document.getElementById('spotify').height="200";
+    counter=true;
+    }
+});
