fdf34001877e7e4b5d56fb34bb3c52124db896de
charb <30449853+aleccharb@users.noreply.github.com>
2017-11-03 16:43:52 -0400

    adf

---
+++ WebbyWeb/Migrations/20171103201737_EditOne.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebbyWeb.Migrations
+{
+    public partial class EditOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Time",
+                table: "Habit",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Time",
+                table: "Habit",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT");
+        }
+    }
+}
